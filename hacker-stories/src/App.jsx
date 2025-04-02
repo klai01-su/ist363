b37876a5f5119a1ee5d597949c73e048a66728d9
klai01-su <klai01@syr.edu>
2025-04-02 03:23:04 -0400

    Update App.jsx

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -1,53 +1,126 @@
-// global variable - every components can see it
-const myGlobalVariable = 'Global Variable';
+// // global variable - every components can see it
+// const myGlobalVariable = 'Global Variable';
+
+// function App() {
+//   // local variable - only works inside the component
+//   const myLocalVariable = 'Local Variable';
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//       <p>{myGlobalVariable}</p>
+//       <p>{myLocalVariable}</p>
+//       <p>separate<br/>lines</p>
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// 1. 
+// // arrow function long form
+// const Hello2 = () => {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//     </div>
+//   );
+// }
+
+// // arrow function concise form
+// const Hello3 = () => ( <div><h1>Hello World</h1></div> ); 
+
+// function App() {
+//   return (
+//     <div>
+//       <h1>Hello World</h1>
+//       <Hello2 />
+//       <Hello3 />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// 2. 
+// function App () {
+//   const handleChange = (event) => {
+//     // whole event
+//     console.log(event);
+//     // value of the typing
+//     console.log(event.target.value);
+//   };
+  
+//   return (
+//     <div>
+//       <label htmlFor="search">Search: </label>
+//       <input type="text" id="search" onChange={handleChange} />
+//     </div>
+//   );
+// }
+
+// export default App;
+
+
+// 3.
+// // bring in react useState code
+// import { useState } from 'react';
+
+// // sets up useState, what variable, what function changes it, and what start value
+// const Home = () => {
+//   const [name, setName] = useState("Kaz");
+
+//   // function actually making dynamic change
+//   let handleClick = () => {
+//     setName ("John");
+//   }
+
+//   return (
+//     <div className = "home">
+//       <h1>Homepage</h1>
+//       <p>{ name }</p>
+//       <button onClick={handleClick}>Update </button>
+//     </div>
+//   );
+// }
+
+// export default Home;
+
+
+// 4.
+const students = [
+  { suid: 123456, name: "Sue Flay", year: "senior", major: "Applied Data Analytics" },
+  { suid: 234567, name: "Ella Vader", year: "junior", major: "Information Management and Technology" },
+  { suid: 345678, name: "Chris P Bacon", year: "junior", major: "Innovation, Society and Technology" },
+];
 
 function App() {
-  // local variable - only works inside the component
-  const myLocalVariable = 'Local Variable';
+  let filteredStudents = students;
+
+  const handleChange = (event) => {
+    filteredStudents = students.filter(student => student.name == event.target.value);
+    console.log(filteredStudents);
+  };
+  
   return (
     <div>
-      <h1>Hello World</h1>
-      <p>{myGlobalVariable}</p>
-      <p>{myLocalVariable}</p>
-      <p>separate<br/>lines</p>
+      <label htmlFor="search">Search: </label>
+      <input type="text" id="search" onChange={handleChange} />
+      <h1>Students</h1>
+      <ul>
+        {
+        filteredStudents.map((item) => {
+        return
+        <li key={item.suid}>
+          Name: {item.name}<br/>
+          Year: {item.year}<br/>
+          Major: {item.major}
+        </li>
+        })}
+      </ul>
     </div>
   );
 }
 
 export default App;
-
-// import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-// import './App.css'
-
-// function App() {
-//   const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vite.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
-// }
-
-// export default App
