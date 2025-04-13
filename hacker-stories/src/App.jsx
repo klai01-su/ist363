5c104d0c51c45efa94b42170b655625877e3ec7c
klai01-su <klai01@syr.edu>
2025-04-13 17:35:55 -0400

    notes

--- hacker-stories/src/App.jsx
+++ hacker-stories/src/App.jsx
@@ -89,37 +89,96 @@
 
 
 // 4.
-const students = [
-  { suid: 123456, name: "Sue Flay", year: "senior", major: "Applied Data Analytics" },
-  { suid: 234567, name: "Ella Vader", year: "junior", major: "Information Management and Technology" },
-  { suid: 345678, name: "Chris P Bacon", year: "junior", major: "Innovation, Society and Technology" },
-];
+// import { useState } from "react";
 
-function App() {
-  let filteredStudents = students;
+// // NEW: modularize our search > takes a call back as a prop
+// const Search = ({ onSearch }) => (
+//   <div>
+//     <label htmlFor="search">Search: </label>
+//     {/* onSearch prop is the passed function to handle the change */}
+//     <input id="search" type="text" onChange={onSearch} />
+//   </div>
+// );
 
-  const handleChange = (event) => {
-    filteredStudents = students.filter(student => student.name == event.target.value);
-    console.log(filteredStudents);
-  };
+// const List = (props) => (
+//   <ul>
+//     {props.list.map((item) => (
+//       <li key={item.suid}>{item.name} {item.year} {item.major}</li>
+//     ))}
+//   </ul>
+// );
+
+// function App() {
+//   // moved out of the global scope
+//   const students = [
+//     {suid: 123456, name: 'Sue Flay', year: 'senior', major: 'Applied Data Analytics'}, 
+//     {suid: 234567, name: 'Ella Vader', year: 'junior', major: 'Information Management and Technology'}, 
+//     {suid: 345678, name: 'Chris P Bacon', year: 'junior', major: 'Innovation, Society and Technology'}
+//   ];
+
+//   let [filteredStudents, setFilteredStudents] = useState(students);
+
+//   // This handler function stays here because state lives in App
+//   const handleChange = (event) => {
+//     setFilteredStudents(
+//       students.filter(student => 
+//         student.name.toLowerCase().includes(event.target.value.toLowerCase())
+//       )
+//     );}
   
+//   return (
+//   <div>
+//     {/* NEW: Pass the handler function down to Search component as a prop */}
+//     <Search onSearch={handleChange} />
+//     <h1>Students</h1>
+//     {/* passing the students object to the list componenet */}
+//     <List list={filteredStudents} />
+//   </div>
+//       );
+//     };  
+
+// export default App;
+
+
+// 5.
+import { useState } from "react";
+import About from './About';
+// ./ means same folder as the App.jsx file
+
+function Home() {
   return (
     <div>
-      <label htmlFor="search">Search: </label>
-      <input type="text" id="search" onChange={handleChange} />
-      <h1>Students</h1>
-      <ul>
-        {
-        filteredStudents.map((item) => {
-        return
-        <li key={item.suid}>
-          Name: {item.name}<br/>
-          Year: {item.year}<br/>
-          Major: {item.major}
-        </li>
-        })}
-      </ul>
+      <h1>Home Page</h1>
     </div>
+  );
+}
+
+// conditional rendering aka navigation SPAs
+function App() {
+  const [page, setPage] = useState("home");
+
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div className="container-fluid">
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item">
+              <button className="nav-link btn btn-link text-light" onClick={() => setPage("home")}>
+                Home
+              </button>
+            </li>
+            <li className="nav-item">
+              <button className="nav-link btn btn-link text-light" onClick={() => setPage("about")}>
+                About
+              </button>
+            </li>
+          </ul>
+        </div>
+      </nav>
+
+      {page === "home" && <Home />}
+      {page === "about" && <About />}
+    </>
   );
 }
 
